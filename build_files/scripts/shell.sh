9e6b6069cfccc7175fd487d434d8706626689f60
Nick Fraser <icanlosh@gmail.com>
2025-08-30 13:45:48 +0100

    [shell] Added podman compose

--- build_files/scripts/shell.sh
+++ build_files/scripts/shell.sh
@@ -4,11 +4,12 @@ set -ouex pipefail
 
 if [[ BUILD_SHELL -eq "1" ]]; then
     dnf5 install -y \
-        screen \
         git \
         git-lfs \
-        qpdf \
-        vim \
         htop \
-        p7zip
+        p7zip \
+        podman-compose \
+        qpdf \
+        screen \
+        vim
 fi
