97a6df0c9318702130363bbd18d138437944d1fb
Nick Fraser <icanlosh@gmail.com>
2025-10-18 14:19:15 +0100

    [docker] Added docker install script

---
+++ build_files/scripts/docker.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+if [[ BUILD_DOCKER -eq "1" ]]; then
+    # Instructions from: https://docs.docker.com/engine/install/fedora/
+    # Remove packages that clash with official docker package
+    dnf5 remove -y \
+        docker \
+        docker-client \
+        docker-client-latest \
+        docker-common \
+        docker-latest \
+        docker-latest-logrotate \
+        docker-logrotate \
+        docker-selinux \
+        docker-engine-selinux \
+        docker-engine
+
+    # Install docker repo
+    dnf5 -y install dnf-plugins-core
+    dnf5 config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    dnf5 config-manager setopt docker-ce-stable.enabled=0
+
+    # Install docker
+    dnf5 install -y \
+        --enable-repo="docker-ce-stable" \
+        docker-ce \
+        docker-ce-cli \
+        containerd.io \
+        docker-buildx-plugin \
+        docker-compose-plugin
+
+    # Enable docker
+    systemctl enable docker
+fi
