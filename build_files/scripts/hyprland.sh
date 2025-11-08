0dcfc35cceb23c86f43685543b5ff6becc8d12e2
Nick Fraser <icanlosh@gmail.com>
2025-11-08 23:15:39 +0000

    [hyprland] installa hyprland from COPR

--- build_files/scripts/hyprland.sh
+++ build_files/scripts/hyprland.sh
@@ -3,17 +3,29 @@
 set -ouex pipefail
 
 if [[ BUILD_HYPRLAND -eq "1" ]]; then
-    dnf5 install -y \
+    dnf5 -y copr enable solopasha/hyprland
+    dnf5 -y install \
         blueman \
         brightnessctl \
         grimshot \
         hyprland \
-        network-manager-applet \
-        pavucontrol \
-        swaylock \
+        hyprpaper \
+        hyprpicker \
+        hypridle \
+        hyprlock \
+        hyprsunset \
+        hyprpolkitagent \
+        hyprsysteminfo \
+        hyprpanel \
+        hyprland-guiutils \
+        hyprland-qt-support	\
+        qt6ct-kde			\
         terminator \
         tesseract \
+        network-manager-applet \
+        pavucontrol \
         waybar \
         wofi \
         xdg-desktop-portal-hyprland
+    dnf5 -y copr disable solopasha/hyprland
 fi
