8f608286704c814f9576cab75f65e9570bf2493e
Nick Fraser <icanlosh@gmail.com>
2026-07-14 22:39:02 +0100

    [plasma] Only apply 1 fix

--- build_files/scripts/plasma-login.sh
+++ build_files/scripts/plasma-login.sh
@@ -9,4 +9,7 @@ SOURCEDIR="${BASEDIR}/etc/systemd/user/plasma-login-kwin_wayland.service.d"
 
 mkdir -p "${DROPINDIR}"
 cp "${SOURCEDIR}/10-force-sw-cursor.conf" "${DROPINDIR}/10-force-sw-cursor.conf"
-cp "${SOURCEDIR}/20-prefer-amd-primary.conf" "${DROPINDIR}/20-prefer-amd-primary.conf"
+
+# Leave the hybrid-GPU ordering override out of the default image for now so
+# we can validate the software-cursor workaround independently first.
+rm -f "${DROPINDIR}/20-prefer-amd-primary.conf"
