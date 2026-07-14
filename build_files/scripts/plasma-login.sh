88183e09c826d1f64606ee5eeecf4c3ae397c54f
Nick Fraser <icanlosh@gmail.com>
2026-07-14 22:36:02 +0100

    [plasma] Added workaround for stuck cursor issue during login

---
+++ build_files/scripts/plasma-login.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+SCRIPTDIR="$(dirname "$(realpath "$0")")"
+BASEDIR="$(realpath "${SCRIPTDIR}/..")"
+DROPINDIR="/usr/lib/systemd/user/plasma-login-kwin_wayland.service.d"
+SOURCEDIR="${BASEDIR}/etc/systemd/user/plasma-login-kwin_wayland.service.d"
+
+mkdir -p "${DROPINDIR}"
+cp "${SOURCEDIR}/10-force-sw-cursor.conf" "${DROPINDIR}/10-force-sw-cursor.conf"
+cp "${SOURCEDIR}/20-prefer-amd-primary.conf" "${DROPINDIR}/20-prefer-amd-primary.conf"
